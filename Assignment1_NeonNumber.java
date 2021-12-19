8b21efadb67220df4e91bc87ede80c051186d890
KeshavLaddha16 <96363330+KeshavLaddha16@users.noreply.github.com>
2021-12-19 23:55:21 +0530

    Add files via upload

---
+++ Assignment1_NeonNumber.java
@@ -0,0 +1,36 @@
+//Assignment 1 --> Neon Number
+package com.mpstme;
+
+import java.util.Scanner;
+
+public class Assignment1_NeonNumber {
+
+    public static void main(String[] args) {
+
+        int num;
+        int sqnum;
+        int sum=0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number: " );
+        num=sc.nextInt();
+
+        //take square of given number
+        sqnum=num*num;
+
+        //loop executes till sqnum comes out to be zero
+        while(sqnum>0)
+            {
+                sum = sum + sqnum % 10;
+                sqnum = sqnum / 10;
+
+            }
+
+        //condition for checking sum is equal or not.
+        if (sum == num)
+            System.out.println("Its a Neon number.");
+        else
+            System.out.println("Its not a Neon number.");
+    }
+}
