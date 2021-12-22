3d1a331905ee3944f60c2f0d333ba22fddc5b508
KeshavLaddha16 <96363330+KeshavLaddha16@users.noreply.github.com>
2021-12-22 20:10:32 +0530

    Add files via upload

---
+++ Assignment2/Assignment2_string_removewhitespaces.java
@@ -0,0 +1,14 @@
+package com.mpstme;
+
+public class Assignment2_string_removewhitespaces {
+
+    public static void main(String[] args) {
+
+        String str1="Remove white spaces";
+
+        //Removes the white spaces using regex
+        str1 = str1.replaceAll("\\s+", "");
+
+        System.out.println("String after removing all the white spaces : " + str1);
+    }
+}
